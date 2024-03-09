3c837b90544a8f79335c23e2097130a67dd6ed99
Atul Rajput <eatul@gmail.com>
2024-03-09 21:30:15 +0530

    Update Repository

---
+++ helloworld.py
@@ -0,0 +1,28 @@
+'''
+Hey, we are learning python today
+Author: Atul Rajput
+Date: 4th March, 2024
+Course: 100DaysCodeChallenge
+'''
+"""
+Hey, we are learning python today
+Author: Atul Rajput
+Date: 4th March, 2024
+Course: 100DaysCodeChallenge
+"""
+# WAP to print hello world
+print("Hello world")
+
+# WAP program and use newline
+print("Hey, I am learning Python\nfrom YouTube by CodewithHarry")
+'''
+This is a multi-line comment
+'''
+"""
+This is also you caan write multi-line comment
+"""
+
+print("Hello guys, good morning! \"Anurag\"\n is my one of the school friends")
+
+print("Hey", 6, 7, sep="~", end="009\n")
+print("Hellen")
---
+++ program2.py
@@ -0,0 +1,59 @@
+# Date: 9th March, 2024
+# Author: Atul Rajput
+# Place: Library
+
+# Problem Statement: Write a program to print Twinkle Twinkle little star poem in python
+
+# Method 1
+print("Twinkle, twinkle, little star,")
+print("How I wonder what you are!")
+print("Up above the world so high,")
+print("Like a diamond in the sky.")
+print("")
+print("When the blazing sun is gone,")
+print("When he nothing shines upon,")
+print("Then you show your little light,")
+print("Twinkle, twinkle, all the night.")
+print("")
+print("Then the traveler in the dark")
+print("Thanks you for your tiny spark,")
+print("How could he see where to go,")
+print("If you did not twinkle so?")
+print("")
+print("In the dark blue sky you keep,")
+print("Often through my curtains peep")
+print("For you never shut your eye,")
+print("Till the sun is in the sky.")
+print("")
+print("As your bright and tiny spark")
+print("Lights the traveler in the dark,")
+print("Though I know not what you are,")
+print("Twinkle, twinkle, little star.")
+
+# Method 2
+print('''
+Twinkle, twinkle, little star,
+How I wonder what you are!
+Up above the world so high,
+Like a diamond in the sky.
+
+When the blazing sun is gone,
+When he nothing shines upon,
+Then you show your little light,
+Twinkle, twinkle, all the night.
+
+Then the traveler in the dark
+Thanks you for your tiny spark,
+How could he see where to go,
+If you did not twinkle so?
+
+In the dark blue sky you keep,
+Often through my curtains peep
+For you never shut your eye,
+Till the sun is in the sky.
+
+As your bright and tiny spark
+Lights the traveler in the dark,
+Though I know not what you are,
+Twinkle, twinkle, little star.
+''')
